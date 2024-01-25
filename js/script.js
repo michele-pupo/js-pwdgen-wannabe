2d087795ccaf14eac08455a2e6b5b8d4d491277b
michele-pupo <mikypupo@hotmail.it>
2024-01-25 14:55:03 +0100

    password generated

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-// Chiediamo il nome con un prompt
+// chiediamo il nome con un prompt
 let firstname = prompt("Qual è il tuo nome?");
 
 document.getElementById("first-name").innerHTML = firstname;
 
-// Chiediamo il cognome con un prompt
-let secondname = prompt("Qual'è il tuo cognome?");
+// chiediamo il cognome con un prompt
+let secondname = prompt("Qual è il tuo cognome?");
 
 document.getElementById("second-name").innerHTML = secondname;
+
+// chiediamo il colore preferito con un prompt
+let color = prompt("Qual è il tuo colore preferito?");
+
+document.getElementById("color-favorite").innerHTML = color;
+
+// generiamo la password con una somma
+document.getElementById("password").innerHTML = `PASSWORD: ${firstname}${secondname}${color}`
