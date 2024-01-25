f0aa49fcb7a7fc009f767e012d79ae80f95914f4
michele-pupo <mikypupo@hotmail.it>
2024-01-25 14:39:09 +0100

    created prompt for first and last name

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Chiediamo il nome con un prompt
+let firstname = prompt("Qual è il tuo nome?");
+
+document.getElementById("first-name").innerHTML = firstname;
+
+// Chiediamo il cognome con un prompt
+let secondname = prompt("Qual'è il tuo cognome?");
+
+document.getElementById("second-name").innerHTML = secondname;
